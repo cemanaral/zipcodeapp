bd403a8199e461b398f667ac2733a69eb9ec4b03
Cem Anaral <36343570+cemanaral@users.noreply.github.com>
2020-01-16 18:19:28 +0300

    Add files via upload

---
+++ zipcodeapp.py
@@ -0,0 +1,39 @@
+def find_name(line):
+    start = 9
+    stop = line.index(' ZIP')
+    name = line[start: stop + 1]
+    return name
+
+
+def find_pop(line):
+    start = line.index('Total population</dt><dd>') + 25
+    c = 0
+    population = ''
+    while line[c + start] != '<':
+        char = line[c + start]
+        if char.isdigit() or char == ',':
+            population += char
+        c += 1
+    return population
+
+
+def main():
+    import urllib.request
+    in_zip = input('Please enter your zip code: ')
+    zip_url = 'http://www.uszip.com/zip/{}'.format(in_zip)
+    zip_url = urllib.request.urlopen(zip_url)
+    for line in zip_url:
+        line = str(line)
+        if "b'<title>" in line:
+            name = find_name(line)
+        elif 'Total population' in line:
+            population = find_pop(line)
+    print('Name: {1}\nPopulation: {0}'.format(population, name))
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except:
+        print('Something went wrong')
+        exit()
